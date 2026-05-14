b75bd40c68eff03761be1745e5c59eccc78edc14
Burgas <burgassme@gmail.com>
2026-05-14 20:43:59 +0700

    Adding Kafka.kt

--- src/main/kotlin/kafka/Kafka.kt
+++ src/main/kotlin/kafka/Kafka.kt
@@ -2,9 +2,17 @@ package org.burgas.kafka
 
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import io.confluent.kafka.serializers.KafkaAvroSerializer
-import io.github.flaxoos.ktor.server.plugins.kafka.*
+import io.github.flaxoos.ktor.server.plugins.kafka.TopicName
+import io.github.flaxoos.ktor.server.plugins.kafka.common
 import io.github.flaxoos.ktor.server.plugins.kafka.components.fromRecord
-import io.ktor.client.*
+import io.github.flaxoos.ktor.server.plugins.kafka.consumer
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerConfig
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerRecordHandler
+import io.github.flaxoos.ktor.server.plugins.kafka.installKafka
+import io.github.flaxoos.ktor.server.plugins.kafka.producer
+import io.github.flaxoos.ktor.server.plugins.kafka.registerSchemas
+import io.github.flaxoos.ktor.server.plugins.kafka.topic
+import io.ktor.client.HttpClient
 import io.ktor.server.application.*
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
@@ -12,47 +20,39 @@ import org.burgas.dto.IdentityResponse
 
 fun Application.configureKafka() {
 
-    install(Kafka) {
-        schemaRegistryUrl = "http://localhost:8081"
+    installKafka {
+        this.schemaRegistryUrl = "http://localhost:8081"
 
-        val identityTopic = TopicName.named("identity-topic")
-        topic(identityTopic) {
-            partitions = 1
-            replicas = 1
-            configs {
-                messageTimestampType = MessageTimestampType.CreateTime
-            }
+        val identityTopic = TopicName("identity-topic")
+        this.topic(identityTopic) {
+            this.partitions = 1
+            this.replicas = 1
         }
 
-        common {
-            bootstrapServers = listOf("localhost:9092")
-            retries = 1
-            clientId = "my-client-id"
+        this.common {
+            this.bootstrapServers = listOf("localhost:9092")
         }
 
-        producer {
-            clientId = "my-client-id"
-            keySerializerClass = StringSerializer::class.java
-            valueSerializerClass = KafkaAvroSerializer::class.java
+        this.producer {
+            this.keySerializerClass = StringSerializer::class.java
+            this.valueSerializerClass = KafkaAvroSerializer::class.java
         }
 
-        consumer {
-            groupId = "my-group-id"
-            clientId = "my-client-id-override"
-            keyDeserializerClass = StringDeserializer::class.java
-            valueDeserializerClass = KafkaAvroDeserializer::class.java
+        this.consumer {
+            this.keyDeserializerClass = StringDeserializer::class.java
+            this.valueDeserializerClass = KafkaAvroDeserializer::class.java
         }
 
-        consumerConfig {
-            consumerRecordHandler(identityTopic) { record ->
+        this.registerSchemas {
+            this.using { HttpClient() }
+            IdentityResponse::class at identityTopic
+        }
+
+        this.consumerConfig {
+            this.consumerRecordHandler(identityTopic) { record ->
                 val identityResponse = fromRecord<IdentityResponse>(record = record.value())
                 println("${record.topic()} :: $identityResponse")
             }
-        }
-
-        registerSchemas {
-            using { HttpClient() }
-            IdentityResponse::class at identityTopic
         }
     }
 }
